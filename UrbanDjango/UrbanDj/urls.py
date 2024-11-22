5e161ef056e2d49b54a7552ad0727dc06efe19a2
Vladdracoola <vladislav_pv@mail.ru>
2024-11-22 22:29:30 +0700

    1

--- UrbanDjango/UrbanDj/urls.py
+++ UrbanDjango/UrbanDj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UrbanDjango project.
+URL configuration for UrbanDj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from UrbanDjango.example1.views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('index/', TemplateView.as_view(template_name='index2.html')),
 ]
